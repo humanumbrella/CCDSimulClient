41c89878c27511dad21958e3cf2168f8faa648b6
humanumbrella <humanumbrella@gmail.com>
2017-02-09 12:59:21 -0800

    Initial Commit
    Code push

---
+++ CCDSimulClient/ClientForm.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace CCDSimulClient
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbIp = new System.Windows.Forms.TextBox();
+            this.btnCon = new System.Windows.Forms.Button();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tbSend = new System.Windows.Forms.TextBox();
+            this.tbLog = new System.Windows.Forms.TextBox();
+            this.btnDiscon = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setSavePathToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnGCPFS = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbIp
+            // 
+            this.tbIp.Location = new System.Drawing.Point(7, 25);
+            this.tbIp.Name = "tbIp";
+            this.tbIp.Size = new System.Drawing.Size(100, 20);
+            this.tbIp.TabIndex = 1;
+            this.tbIp.Text = "139.229.21.138";
+            // 
+            // btnCon
+            // 
+            this.btnCon.Location = new System.Drawing.Point(219, 24);
+            this.btnCon.Name = "btnCon";
+            this.btnCon.Size = new System.Drawing.Size(75, 23);
+            this.btnCon.TabIndex = 2;
+            this.btnCon.Text = "Connect";
+            this.btnCon.UseVisualStyleBackColor = true;
+            this.btnCon.Click += new System.EventHandler(this.btnCon_Click);
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(114, 26);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(100, 20);
+            this.tbPort.TabIndex = 3;
+            this.tbPort.Text = "37204";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(382, 147);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // tbSend
+            // 
+            this.tbSend.Location = new System.Drawing.Point(11, 150);
+            this.tbSend.Name = "tbSend";
+            this.tbSend.Size = new System.Drawing.Size(365, 20);
+            this.tbSend.TabIndex = 5;
+            // 
+            // tbLog
+            // 
+            this.tbLog.Location = new System.Drawing.Point(12, 54);
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.ReadOnly = true;
+            this.tbLog.Size = new System.Drawing.Size(364, 78);
+            this.tbLog.TabIndex = 6;
+            // 
+            // btnDiscon
+            // 
+            this.btnDiscon.Enabled = false;
+            this.btnDiscon.Location = new System.Drawing.Point(300, 24);
+            this.btnDiscon.Name = "btnDiscon";
+            this.btnDiscon.Size = new System.Drawing.Size(75, 23);
+            this.btnDiscon.TabIndex = 7;
+            this.btnDiscon.Text = "Disconnect";
+            this.btnDiscon.UseVisualStyleBackColor = true;
+            this.btnDiscon.Click += new System.EventHandler(this.btnDiscon_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(548, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setSavePathToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // setSavePathToolStripMenuItem
+            // 
+            this.setSavePathToolStripMenuItem.Name = "setSavePathToolStripMenuItem";
+            this.setSavePathToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.setSavePathToolStripMenuItem.Text = "Set Save Path";
+            this.setSavePathToolStripMenuItem.Click += new System.EventHandler(this.setSavePathToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // btnGCPFS
+            // 
+            this.btnGCPFS.Enabled = false;
+            this.btnGCPFS.Location = new System.Drawing.Point(382, 112);
+            this.btnGCPFS.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGCPFS.Name = "btnGCPFS";
+            this.btnGCPFS.Size = new System.Drawing.Size(148, 19);
+            this.btnGCPFS.TabIndex = 9;
+            this.btnGCPFS.Text = "GetClientPathFromServer";
+            this.btnGCPFS.UseVisualStyleBackColor = true;
+            this.btnGCPFS.Click += new System.EventHandler(this.btnGCPFS_Click);
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 277);
+            this.Controls.Add(this.btnGCPFS);
+            this.Controls.Add(this.btnDiscon);
+            this.Controls.Add(this.tbLog);
+            this.Controls.Add(this.tbSend);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.btnCon);
+            this.Controls.Add(this.tbIp);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ClientForm";
+            this.Text = "CCDSimulClient";
+            this.Load += new System.EventHandler(this.btnCon_Click);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbIp;
+        private System.Windows.Forms.Button btnCon;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tbSend;
+        private System.Windows.Forms.TextBox tbLog;
+        private System.Windows.Forms.Button btnDiscon;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setSavePathToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnGCPFS;
+    }
+}
+
---
+++ CCDSimulClient/ClientForm.cs
@@ -0,0 +1,445 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using CCDSimulServer;
+using System.Diagnostics;
+using System.IO;
+namespace CCDSimulClient
+{
+
+    public partial class ClientForm : Form
+    {
+        
+        private Socket _clientSocket;
+        private int port = 0;
+        private byte[] _buffer;
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+        private string maximpath = @"C:\Program Files (x86)\Diffraction Limited\MaxIm DL V5\MaxIm_DL.exe";
+        //for p8 uncomment
+        //private string maximpath = @"C:\Program Files\Diffraction Limited\MaxIm DL V5\MaxIm_DL.exe";
+        
+        private string savepath = @"C:\Users\jmoore\Dropbox\Polarimetry\";
+        //for p8 uncomment
+        //private string savepath = @"C:\Skynet-Testing\";
+
+        private ushort epsilon = 0;
+        private double expTime = 0;
+        private bool isDark;
+        private bool saveImages;
+        private bool clientSetSavePath = false;
+
+        private string filter = string.Empty;
+        private int orderPos = -1;
+        
+
+        private MaxIm.CCDCamera ccd;
+
+        public ClientForm()
+        {
+            InitializeComponent();
+            //if maxim isn't open - let's open it.
+            Process[] localByName = Process.GetProcessesByName("MaxIm_DL");
+            /*
+            if (localByName.Length == 0)
+            {
+
+                Process.Start(maximpath);
+            }*/
+
+            while (true)
+            {
+                try
+                {
+                    if (localByName.Length == 0)
+                    {
+                        Process.Start(maximpath);
+                        break;
+                    }
+                    break;
+
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.ToString());
+                    MessageBox.Show("Choose folder where MaximDL lives...");
+                    DialogResult result = fbd.ShowDialog();
+                    if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    {
+                        maximpath = fbd.SelectedPath + "\\MaxIm_DL.exe";
+                    }
+                }
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            if (ccd.LinkEnabled == true)
+            {
+                ccd.CoolerOn = false;
+                ccd.LinkEnabled = false;
+            }
+
+
+            if (_clientSocket != null && !_clientSocket.IsBound)
+            {
+                _clientSocket.Shutdown(SocketShutdown.Both);
+                _clientSocket.Close();
+            }
+        }
+
+        private void btnCon_Click(object sender, EventArgs e)
+        {
+            _clientSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            Int32.TryParse(tbPort.Text, out port);
+            _clientSocket.BeginConnect(new IPEndPoint(IPAddress.Parse(tbIp.Text), port), new AsyncCallback(connectCallback), null);
+            toggleConnect(true);
+        }
+
+        private void connectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                _clientSocket.EndConnect(ar);
+                AppendToTextBox("Connected to server!");
+                btnSend.Enabled = true;
+                _buffer = new byte[_clientSocket.ReceiveBufferSize];
+                _clientSocket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(receiveCallback), null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void receiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                int received = _clientSocket.EndReceive(ar);
+
+                if (received == 0)
+                {
+                    IPEndPoint clientIP = (IPEndPoint)_clientSocket.LocalEndPoint;
+                    AppendToTextBox("Server " + clientIP.Address.ToString() + " disconnected!");
+                    return; //eliminate the beginreceive below... bc client likely disconnected
+                }
+                Array.Resize(ref _buffer, received);
+
+                string cmd = Encoding.ASCII.GetString(_buffer);
+                if (cmd.Equals("setup_ccd"))
+                {
+
+                    ccd = new MaxIm.CCDCamera();
+
+                    ccd.DisableAutoShutdown = true;
+                    ccd.LinkEnabled = true;
+                    ccd.CoolerOn = true;
+                    AppendToTextBox("Client Camera Is Ready!");
+                    sendToServer("Client Camera Is Ready!");
+                }
+                else if (cmd.Equals("teardown_ccd"))
+                {
+                    try
+                    {
+                        ccd.CoolerOn = false;
+                        ccd.LinkEnabled = false;
+                        
+                        AppendToTextBox("Client Camera Is Disconnected!");
+                        sendToServer("Client Camera Is Disconnected!");
+                    }
+                    catch (Exception ex)
+                    {
+                        AppendToTextBox("Cant Connect To Client Camera");
+                        sendToServer("Cant Connect To Client Camera");
+                        MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                }
+                else { 
+                    ExposePackage recvd = new ExposePackage(_buffer);
+
+                    epsilon = recvd.epsilon;
+                    expTime = recvd.expTime;
+                    isDark = recvd.isDark;
+                    saveImages = recvd.saveImages;
+                    if (!clientSetSavePath)
+                    {
+                        savepath = recvd.savepath;
+
+                    }
+                    filter = recvd.filter;
+                    orderPos = recvd.orderPos;
+
+                    waitFor(epsilon);
+                    exposeFor(isDark, expTime);
+
+                    while (!ccd.ImageReady)
+                    {
+                        //wait
+                    }
+
+                    string dt = string.Empty;
+                    double jd = 0.0;
+                    dt = ccd.Document.GetFITSKey("DATE-OBS");
+                    AppendToTextBox("client_cam: " + dt);
+                    sendToServer(" client_cam: " + dt);
+                    jd = ccd.Document.GetFITSKey("JD");
+                    AppendToTextBox("client_cam: " + jd.ToString());
+                    sendToServer(" client_cam: " + jd.ToString());
+                }
+                /*
+                string text = Encoding.ASCII.GetString(_buffer);
+                //handle the packet
+                AppendToTextBox(text);
+                handlePacket(text);*/
+                Array.Resize(ref _buffer, _clientSocket.ReceiveBufferSize);
+                 
+                //restart receive
+                _clientSocket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(receiveCallback), null);
+
+            }
+            catch (Exception ex)
+            {
+                AppendToTextBox("Server Disconnected!");
+                if (!ex.Message.Contains("Cannot access a disposed object"))
+                {
+                    MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+        }
+
+        private void handlePacket(string text)
+        {
+            switch (text)
+            {
+                case "setup_ccd":
+                    try
+                    {
+                        ccd = new MaxIm.CCDCamera();
+                        ccd.LinkEnabled = true;
+                        AppendToTextBox("Client Camera Is Ready!");
+                        sendToServer("Client Camera Is Ready!");
+                    }
+                    catch (Exception ex)
+                    {
+                        AppendToTextBox("Cant Connect To Client Camera");
+                        sendToServer("Cant Connect To Client Camera");
+                        MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                    break;
+
+                case "teardown_ccd":
+                    try
+                    {
+                        ccd.LinkEnabled = false;
+                        AppendToTextBox("Client Camera Is Disconnected!");
+                        sendToServer("Client Camera Is Disconnected!");
+                    }
+                    catch (Exception ex)
+                    {
+                        AppendToTextBox("Cant Connect To Client Camera");
+                        sendToServer("Cant Connect To Client Camera");
+                        MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                    break;
+                case "expose_ccd":
+                    ccd.Expose(1, 1, 0);
+
+                    while (!ccd.ImageReady)
+                    {
+                        //wait
+                    }
+                    AppendToTextBox("Client Exposure Is Done!");
+                    sendToServer("Client Exposure Is Done!");
+                    break;
+                default:
+                    break;
+
+            }
+        
+        
+        }
+
+        private void waitFor(ushort s)
+        {
+            Thread.Sleep(s*1000);
+        }
+        private void exposeFor(bool dark, double exp)
+        {
+
+            ccd.Expose((double)exp, Convert.ToInt16(!dark));
+
+            while (!ccd.ImageReady)
+            {
+                //wait
+            }
+            sendToServer("Client Camera Exposure Is Done!");
+
+            string dt = string.Empty;
+            dt = ccd.Document.GetFITSKey("DATE-OBS");
+            checkForDirectory(savepath);
+            string testing = string.Empty;
+            if (saveImages)
+            {
+                if (orderPos > -1)
+                {
+                    testing = savepath + dt.Replace(":", "-") +"-"+ filter + "-" + orderPos + "-U47.fit";
+
+                }
+                else
+                {
+                    testing = savepath + dt.Replace(":", "-") + "-U47.fit";
+                }
+                ccd.SaveImage(testing);
+                sendToServer("Client Camera Exposure Saved!");
+                sendToServer("Client:" + savepath);
+                AppendToTextBox("Client Camera Exposure Saved!");
+                AppendToTextBox("Client:" + savepath);
+            }
+            else
+            {
+                sendToServer("Client Camera Exposure *not* saved!");
+                AppendToTextBox("Client Camera Exposure *not* saved!");
+            }
+        }
+        private void checkForDirectory(string fp)
+        {
+            if (!Directory.Exists(fp))
+            {
+                Directory.CreateDirectory(fp);
+            }
+        }
+        private void AppendToTextBox(string text)
+        {
+            MethodInvoker invoker = new MethodInvoker(delegate
+            {
+                tbLog.AppendText(text + "\r\n");
+                if (text.Contains("disconnected"))
+                {
+                    toggleConnect(false);
+                    if (ccd != null)
+                    {
+                        ccd.CoolerOn = false;
+                        ccd.LinkEnabled = false;
+                    }
+                }
+            });
+            this.Invoke(invoker);
+        }
+
+        private void clearSendTb()
+        {
+
+            MethodInvoker invoker = new MethodInvoker(delegate
+            {
+                tbSend.Text = "";
+            });
+            this.Invoke(invoker);
+        }
+        private void sendToServer(string toSend)
+        {
+            try
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes(toSend+"\r\n");
+                _clientSocket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(SendCallback), null);
+                clearSendTb();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes(tbSend.Text);
+                _clientSocket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(SendCallback), null);
+                clearSendTb();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                _clientSocket.EndSend(ar);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void toggleConnect(bool change)
+        {
+            if (change)
+            {
+                btnDiscon.Enabled = true;
+                btnCon.Enabled = false;
+            }
+            else
+            {
+                btnDiscon.Enabled = false;
+                btnCon.Enabled = true;
+            }
+        }
+
+        private void btnDiscon_Click(object sender, EventArgs e)
+        {
+            _clientSocket.Shutdown(SocketShutdown.Both);
+            _clientSocket.Close();
+            //bug on close
+            //_clientSocket = null;
+            toggleConnect(false);
+        }
+
+
+        private void setSavePathToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = fbd.ShowDialog();
+            if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                savepath = fbd.SelectedPath + "\\";
+            }
+            clientSetSavePath = true;
+            btnGCPFS.Enabled = true;
+
+
+            AppendToTextBox("Client Path Updated:");
+            AppendToTextBox(savepath + "\n");
+            sendToServer("Client set custom save directory!  Your sends will be ignored unless they uncheck.");
+        }
+
+        private void btnGCPFS_Click(object sender, EventArgs e)
+        {
+            clientSetSavePath = false;
+            btnGCPFS.Enabled = false;
+        }
+
+    }
+}
